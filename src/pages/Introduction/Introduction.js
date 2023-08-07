7070bc37e3f83211cb598fb1b06160b2a8ac83d2
Mostafa Mosavi <mostafamosavi5570@gmail.com>
2023-08-08 00:38:03 +0330

    fix Introduction

--- src/pages/Introduction/Introduction.js
+++ src/pages/Introduction/Introduction.js
@@ -4,28 +4,38 @@ import BlueButton from "../../components/button/BlueButton";
 import global from "../../images/global.png";
 import WhiteButton from "../../components/button/WhiteButton";
 import Import from "../../images/import.png";
-
+import Import1 from "../../images/import1.png"
+import frame from "../../images/Frame.png"
 const Introduction = () => {
     return (
         <div className="circle-box">
             <div className="circle">
-                <div className="inner-div"></div>
+                <div className="inner-circle">
+                    <img src={frame} width={400}/>
+                </div>
             </div>
             <div className="text-box">
                <h1>معرفی اپلیکیشن شوفر</h1>
                <h3>شوفر یک سامانه هوشمند باری درخواست صدور بارنامه میباشد که رانندگان عزیز میتوانند بدون معطلی و پرداخت هزینه کمیسیون اضافی به راحتی بارنامه خود را دریافت کنند </h3>
-                <div className="buttons">
-                    <BlueButton>
+                <div className="buttons" style={{
+                    marginRight:"334px"
+                }}>
+                    <BlueButton >
                     <span className="button-content">
                            ورود به وب اپلیکیشن <img src={global} alt="" width={20} style={{
-                        marginLeft:"10px"
+                        marginLeft:"10px",
+
                     }}/>
                      </span>
                     </BlueButton>
                     <WhiteButton>
-                    <span className="button-content ">
-                    دانلود اپلیکیشن    <img src={Import} alt="" width={20} style={{
-                        marginLeft:"10px"
+                    <span className="button-content" style={{
+                        color:"white"
+                    }}>
+                    دانلود اپلیکیشن    <img src={Import1} alt="" width={20} style={{
+                        marginLeft:"10px",
+                        color:'white',
+
                     }}/></span>
                     </WhiteButton>
                 </div>
